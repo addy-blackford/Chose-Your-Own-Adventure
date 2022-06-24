ffd81b8dbbea05a2034ecac73fdc2372d1aa9b67
addy-blackford <107376846+addy-blackford@users.noreply.github.com>
2022-06-24 14:38:08 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(".characters").click( function() {
   $(".characters").hide();  
   $(".character-text").hide();
-  $(".car-text, button").show();
+  $(".car-text").show();
 });
 
 $(".mario-img").click( function() {
@@ -16,3 +16,34 @@ $(".cars").click(function() {
     $(".power, .power-text").show();
     $(".cars, .car-text").hide();
 });
+
+/* changing power-up classes 
+
+$(".s-star").click(function() {
+    $(".banana").addClass("bna-ss");
+   	$(".mushroom").addClass("mshr-ss"); 
+});
+
+$(".mr").click(function() {
+   	$(".banana").addClass("bna-mr"); 
+    $(".mushroom").addClass("mshr-mr");
+});
+
+$(".polter").click(function() {
+   	$(".banana").addClass("bna-plt"); 
+    $(".mushroom").addClass("mshr-plt"); 
+});
+
+$(".lg").click(function() {
+   	$(".banana").addClass("bna-lg"); 
+    $(".mushroom").addClass("mshr-lg"); 
+});
+
+*/
+
+
+$(".bna-ss").hover(function() {
+	$(".loser").show();
+    $(".power").hide();
+});
+

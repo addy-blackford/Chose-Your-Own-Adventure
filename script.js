0a3f4c2586df2ef11099b6b469f166efc9a7e564
addy-blackford <107376846+addy-blackford@users.noreply.github.com>
2022-06-27 09:15:06 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -16,34 +16,3 @@ $(".cars").click(function() {
     $(".power, .power-text").show();
     $(".cars, .car-text").hide();
 });
-
-/* changing power-up classes 
-
-$(".s-star").click(function() {
-    $(".banana").addClass("bna-ss");
-   	$(".mushroom").addClass("mshr-ss"); 
-});
-
-$(".mr").click(function() {
-   	$(".banana").addClass("bna-mr"); 
-    $(".mushroom").addClass("mshr-mr");
-});
-
-$(".polter").click(function() {
-   	$(".banana").addClass("bna-plt"); 
-    $(".mushroom").addClass("mshr-plt"); 
-});
-
-$(".lg").click(function() {
-   	$(".banana").addClass("bna-lg"); 
-    $(".mushroom").addClass("mshr-lg"); 
-});
-
-*/
-
-
-$(".bna-ss").hover(function() {
-	$(".loser").show();
-    $(".power").hide();
-});
-
